7d9acdb996b0e7ac09476caff480c71d8c44af8d
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-11-02 07:49:11 -0700

    Agregar archivos de proyecto.

---
+++ AplicacionMovil/AplicacionMovil/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AplicacionMovil
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void btnBoton_Clicked(object sender, EventArgs e)
+        {
+            float cantidad = float.Parse(txtCantidad.Text);
+            float diezporcen = cantidad * 0.1f;
+            lblResultado.Text = diezporcen.ToString();
+        }
+    }
+}
